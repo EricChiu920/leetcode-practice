b5be3e7ffd6a994fe2ec925df348fec7c82a2c6f
Eric Chiu <EricChiu920@gmail.com>
2019-09-20 18:08:31 -0400

    add keys and rooms

---
+++ lc-841/lc-841.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+class Solution:
+  def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+    table = [False] * len(rooms)
+    table[0] = True
+    counter = 1
+    queue = deque(rooms[0])
+    
+    while len(queue) > 0:
+      key = queue.popleft()
+      if table[key] is False:
+        table[key] = True
+        counter += 1
+        for new_key in rooms[key]:
+          queue.appendleft(new_key)
+
+    for room in table:
+      if room is False: return False
+    
+    return counter == len(rooms)
